4d78a9c687c904394bb2c9bc2ffd80c7272f8dbc
Matias Rivera <juanmatiasrivera@outlook.com>
2021-01-18 21:59:58 -0300

    auth

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,84 +1,170 @@
+const { errorHandler } = require('../helpers/dbErrorHandler')
+const generateToken = require('../helpers/generateToken')
 const User = require('../models/user')
+
+
+//@desc     Auth user & get token
+//@route    POST /api/users/login
+//@access   Public
+exports.login = (req, res) =>{
+    const {email, password} = req.body
+
+    //check if email is already in use
+    User.findOne({email}).exec((err, user) => {
+        //if user exist and entered password is the same
+        if(user){
+            user.matchPassword(password).then((result) => {
+                if(result){
+                    res.json({
+                        _id: user._id,
+                        name: user.name,
+                        email: user.email,
+                        isAdmin: user.isAdmin,
+                        token: generateToken(user._id)
+                    })
+                }
+                else{
+                    return res.status(400).json({
+                        error: 'Invalid email or password'
+                    })
+                }
+            })
+            
+        }else{
+            return res.status(400).json({
+                error: 'Invalid email or password'
+            })
+        }
+        
+    })
+
+    
+}
+
 
 //@desc     Register a new user
 //@route    POST /api/users
 //@access   Public
-exports.register = async (req, res) =>{
+exports.register = (req, res) =>{
     const {name, email, password} = req.body
 
     //check if email is already in use
-    const userExists = await User.findOne({email})
-    if(userExists){
-        res.status(400)
-        throw new Error('User already exists')
-    }
+    User.findOne({email}).exec((err, user) => {
+        if(err){
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        if(user){
+            return res.status(400).json({
+                error: 'User already exists'
+            })
+        }else{
+            const user = new User({
+                name,
+                email,
+                password
+            })
+            user.save((err, user) => {
+                if(err){
+                    return res.status(400).json({
+                        error: errorHandler(err)
+                    })
+                }
+                res.status(201).json({ 
+                    _id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    isAdmin: user.isAdmin,
+                    token: generateToken(user._id)
+                })
+            })
+        }
+    })
+
 
     
-    const user = await User.create({
-        name,
-        email,
-        password
-    })
-    if(user){
-        //return created user
-        res.status(201).json({ 
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
-           // token: generateToken(user._id)
-        })
-    }else{
-        res.status(400)
-        throw new Error('Invalid user data')
-    }
+
 }
 
 
 //@desc     Get all users
 //@route    GET /api/users
 //@access   Private/admin
-exports.read = async (req, res) =>{
+exports.readAll = async (req, res) =>{
     const users = await User.find({}).select('-password')
     res.json(users)
+}
+
+
+//@desc     Get all users
+//@route    GET /api/users
+//@access   Private/admin
+exports.read = (req, res) =>{
+    User.findById(req.params.id).select('-password').exec((err, user) => {
+        if(err){
+            return res.status(400).json({
+                error: 'User not found'
+            })
+        }
+        return res.json(user)
+    })
+
+    
 }
 
 
 //@desc     Update user
 //@route    PUT /api/users/:id
 //@access   Private/admin
-exports.update = async (req, res) =>{
+exports.update = (req, res) =>{
     
-    const user = await User.findById(req.params.id)
+    User.findById(req.params.id).exec((err, user) => {
+        if(err){
+            return res.status(400).json({
+                error: 'User not found'
+            })
+        }
+        
+            user.name = req.body.name || user.name
+            user.email = req.body.email || user.email
+            user.isAdmin = req.body.isAdmin || user.isAdmin 
+         
+            user.save((err, result) => {
+                if(err){
+                    return res.status(400).json({
+                        error: errorHandler(err)
+                    })
+                }
+                res.json({message:'user was updated successfully'})
+            })
+    
+        
+        
+    })
 
-    if(user){
-        user.name = req.body.name || user.name
-        user.email = req.body.email || user.email
-        user.isAdmin = req.body.isAdmin || user.isAdmin 
-     
-        const updatedUser = await user.save()
-
-        res.json({message:'user was updated successfully'})
-    }else{
-        res.status(404)
-        throw new Error('User not found')
-    }
+    
 }
 
 
 //@desc     Delete user
 //@route    DELETE /api/users/:id
 //@access   Private/admin
-exports.remove = async (req, res) =>{
-    const user = await User.findById(req.params.id)
-    
-    if(user) {
-        await user.remove()
-        res.json({ message: 'User removed'})
-    } else {
-        res.status(404)
-        throw new Error('User not found')
-    }
-
+exports.remove = (req, res) =>{
+    User.findById(req.params.id).exec((err, user) => {
+        if(err){
+            return res.status(400).json({
+                error: 'User not found'
+            })
+        }
+        user.remove().exec((err, result) => {
+            if(err){
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json({ message: 'User removed'})
+        })
+    })
     
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const router = express.Router()
-const {register, read, update, remove} = require('../controllers/user.js')
+const {register, readAll, read, update, remove, login} = require('../controllers/user.js')
+
 
 router.route('/')
     .post(register)
-    .get(read)
+    .get(readAll)
 
 router.route('/:id')
+    .get(read)
     .put(update)
     .delete(remove)
+
+router.route('/login')
+    .post(login)
 
 module.exports = router
